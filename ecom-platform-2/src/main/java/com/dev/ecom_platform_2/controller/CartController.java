876d040dfcd1c2d833b1fd4fbb2533a041a5e033
Alin RADU <alin.radu.cristian@gmail.com>
2025-04-15 17:08:08 +0300

    add test3 for git config, ecom-platform-2

--- ecom-platform-2/src/main/java/com/dev/ecom_platform_2/controller/CartController.java
+++ ecom-platform-2/src/main/java/com/dev/ecom_platform_2/controller/CartController.java
@@ -82,7 +82,7 @@ public class CartController {
             @PathVariable UUID cartId, @PathVariable UUID productId) {
         cartService.deleteProductFromCart(cartId, productId);
 
-        System.out.println("test1");
+        System.out.println("test3");
 
         return ResponseEntity.status(HttpStatus.OK).build();
     }
